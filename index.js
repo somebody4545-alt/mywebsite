dcee0fb6254e2519e874afe06376839b4f4ff473
somebody4545-alt <somebody4545mc+test@gmail.com>
2024-08-14 22:21:12 +0000

    made a basic website

---
+++ index.js
@@ -0,0 +1,8 @@
+var button = document.getElementById("funbutton");
+function change() {
+    if (button.style.backgroundColor == "white") {
+        button.style.backgroundColor = "blue"
+    } else {
+        button.style.backgroundColor = "white"
+    }
+}
